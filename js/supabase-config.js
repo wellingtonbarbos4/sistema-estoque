09aebd367ad7c2b0cd5f0e4e3119df3ed8716c76
wellingtonbarbos4 <wellingtonwbarbos4@gmail.com>
2026-06-30 11:01:12 -0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,5 +1,5 @@
-// Configuração de conexão com o banco de dados
-const SUPABASE_URL = "https://fqycxqprorfuwrjptckv.supabase.co/rest/v1/";
+// Configuração de conexão com o banco de dados (URL Limpa)
+const SUPABASE_URL = "https://fqycxqprorfuwrjptckv.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxeWN4cXByb3JmdXdyanB0Y2t2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxMTU0MzEsImV4cCI6MjA5NzY5MTQzMX0.heywuqU_ONCJzxGs_EI9Dq11VWpL_ctngclf3eaz45c";
 
 // Inicializa o cliente do Supabase globalmente
